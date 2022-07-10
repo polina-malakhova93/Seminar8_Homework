672ed3d309de3a950db759b5e54f56be79e5f286
polina993 <>
2022-07-10 13:11:45 +0300

    add task 54

---
+++ task54/Program.cs
@@ -0,0 +1,52 @@
+﻿int m = Input("Введите количество строк: ");
+int n = Input("Введите количество столбцов: ");
+int[,] numbers = new int[m, n];
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        numbers[i, j] = new Random().Next(1, 100);
+    }
+}
+PrintArray(numbers);
+SortArrayLines(numbers);
+void SortArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+
+Console.WriteLine();
+PrintArray(numbers);
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
